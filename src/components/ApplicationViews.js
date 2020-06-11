7bc0b2d7c33fe3575b9d72241adaae6aecbdbc24
felipetempus <felipemouradev@gmail.com>
2020-06-11 16:18:08 -0400

    messages integrated with the API, working

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,15 +1,18 @@
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import React from "react"
 
 // Moods
 import Welcome from "./Welcome/Welcome"
 import Register from "./Register/Register"
 import Login from "./Login/Login"
+import Messages from "./Messages/Messages"
 
 const ApplicationViews = (props) => {
     // User
     const hasUser = props.hasUser;
     const setUser = props.setUser;
+
+
 
     return (
         <React.Fragment>
@@ -17,23 +20,30 @@ const ApplicationViews = (props) => {
                 exact
                 path="/"
                 render={props => {
-                    return <Welcome {...props}/>;
+                    return <Welcome {...props} />;
                 }}
             />
             <Route
                 exact
                 path="/register"
                 render={props => {
-                    return <Register setUser={setUser} {...props}/>;
+                    return <Register setUser={setUser} {...props} />;
                 }}
             />
             <Route
                 exact
                 path="/login"
                 render={props => {
-                    return <Login setUser={setUser} {...props}/>;
+                    return <Login setUser={setUser} {...props} />;
                 }}
             />
+            <Route exact path="/messages" render={props => {
+                if (hasUser) {
+                    return <Messages {...props} />
+                } else {
+                    return <Redirect to="/login" />
+                }
+            }} />
         </React.Fragment>
     );
 };
---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import {
+    Jumbotron,
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input,
+    Col
+} from 'reactstrap';
+
+// moods
+import API from "../../modules/GeneralModule"
+
+const Messages = (props) => {
+
+    const [message, setMessage] = useState({content:""});
+    const [allMessages, setAllMessages] = useState([]);
+
+    // handle changes on the input message
+    const handleChange = (e) => {
+        let stateToChange = { ...message };
+        stateToChange[e.target.id] = e.target.value;
+        console.log(stateToChange)
+        setMessage(stateToChange)
+    }
+
+    // get all messages to load
+    const getMessages = async (m) => {
+        await setAllMessages(API.getAll("messages"))
+        console.log("get working")
+    }
+
+    // Enter send the message
+    const keyPressed = (e) => e.key === "Enter" ? submitMessage() : null
+
+    // handle the send message
+    const submitMessage = (e) => {
+        API.post("messages", message)
+    }
+
+    return <>
+        <div className="container d-flex justify-content-center">
+            <Jumbotron className="superbox">
+            <h1 className="display-3">Messages</h1>
+            <div className="messages" id="messages">
+                {/* mesSages api */}
+            </div>
+            <div className="form-row">
+                <Input className="" onKeyPress={keyPressed} onChange={handleChange} type="text" name="" id="content" placeholder="Type a message" />
+                <Button onClick={submitMessage}>Send</Button>
+            </div>
+            </Jumbotron>
+        </div>
+    </>
+}
+
+export default Messages
